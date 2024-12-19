9e2938135b1c1364fce06e91cd52348d4d35f933
Paweł Rapacz <apacz_paw@wp.pl>
2024-12-19 22:50:04 +0100

    Modify Phenotype class

--- src/Phenotype.cpp
+++ src/Phenotype.cpp
@@ -2,24 +2,29 @@
 #include <cmath>
 #include <random>
 #include <chrono>
+#include <sstream>
 
 
-Phenotype::Phenotype(std::string_view gnm) {
+Phenotype::Phenotype(std::string_view genome) {
+    std::istringstream gnm_in(genome.data());
+    gene_t g;
+    while (gnm_in >> g)
+        _gnm.push_back(g);
 }
 
 
 
-Phenotype::Phenotype(genome_frac gnm1, genome_frac gnm2) {
-    std::ptrdiff_t gnm1_len = gnm1.first - gnm1.second;
-    std::ptrdiff_t gnm2_len = gnm2.first - gnm2.second;
-    _gnm.reserve(std::abs(gnm1_len) + std::abs(gnm2_len));
-    _gnm.assign(gnm1.first, gnm1.second);
-    _gnm.insert(_gnm.end(), gnm2.first, gnm2.second);
+Phenotype::Phenotype(genome_frac genome1, genome_frac genome2) {
+    std::ptrdiff_t genome1_len = genome1.second - genome1.first;
+    std::ptrdiff_t genome2_len = genome2.second - genome2.first;
+    _gnm.reserve(genome1_len + genome2_len);
+    _gnm.assign(genome1.first, genome1.second);
+    _gnm.insert(_gnm.end(), genome2.first, genome2.second);
 }
 
 
 
-genome_frac Phenotype::frac_begin() const {
+genome_frac Phenotype::frac_front() const {
     static std::default_random_engine rand(std::chrono::system_clock::now().time_since_epoch().count());
     std::uniform_int_distribution<> range(1, _gnm.size() - 1);
     return {_gnm.cbegin(), _gnm.cbegin() + range(rand)};
@@ -32,3 +37,21 @@ genome_frac Phenotype::frac_back() const {
     std::uniform_int_distribution<> range(0, _gnm.size() - 2);
     return {_gnm.cbegin() + range(rand), _gnm.cend()};
 }
+
+
+
+const genome_t &Phenotype::genome() const noexcept
+{ return _gnm; }
+
+
+
+adapt Phenotype::adapt() const noexcept
+{ return _adapt; }
+
+
+
+void Phenotype::adapt(::adapt a) noexcept
+{ _adapt = a; }
+
+void Phenotype::operator=(::adapt a) noexcept
+{ adapt(a); }
--- src/include/Phenotype.h
+++ src/include/Phenotype.h
@@ -1,15 +1,17 @@
 #pragma once
 
-#include <string>
 #include <cstdint>
 #include <vector>
 #include <utility>
 #include <string_view>
 
 
-using gene = uint16_t;
-using genome_t = std::vector<gene>;
+
+using gene_t = uint16_t;
+using genome_t = std::vector<gene_t>;
 using genome_frac = std::pair<genome_t::const_iterator, genome_t::const_iterator>;
+
+
 
 enum class adapt {
     breed,
@@ -17,26 +19,22 @@ enum class adapt {
     dead
 };
 
-class Population;
+
 
 class Phenotype
 {
 private:
-    adapt _adapt;
+    adapt _adapt = adapt::breed;
     genome_t _gnm;
 
-    // inline static std::default_random_engine rand;
 
 public:
-    Phenotype(std::string_view gnm);
-    Phenotype(genome_frac gnm1, genome_frac gnm2);
-    genome_frac frac_begin() const;
+    Phenotype(std::string_view genome);
+    Phenotype(genome_frac genome1, genome_frac genome2);
+    genome_frac frac_front() const;
     genome_frac frac_back() const;
-
-public:
-    const adapt& adpt = _adapt;
-
-
-    friend class Population;
-    friend void write_population(std::ofstream& file, const Population& p);
+    const genome_t& genome() const noexcept;
+    adapt adapt() const noexcept;
+    void adapt(::adapt a) noexcept;
+    void operator=(::adapt a) noexcept;
 };
